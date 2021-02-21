6edd7b61f0ba179a470c66a36311a8711c1515a3
VacantThinker43z4c <hangzhuixiong06648@163.com>
2021-02-21 15:33:03 +0800

    Clean du projet

--- src/main/java/com/karimandco/cv/ConnexionDB.java
+++ src/main/java/com/karimandco/cv/ConnexionDB.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.karimandco.connexion;
+package com.karimandco.cv;
 
 import com.mysql.jdbc.Connection;
 import java.io.File;
